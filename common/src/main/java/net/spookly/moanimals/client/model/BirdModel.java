e45d019832139c85d653ff1d338f72a7316c6750
Niklas Ehlen <niklas.ehlen1@gmail.com>
2026-04-15 22:58:00 +0200

    feat: update BirdModel with new animations, head rotation logic, and dependency versions

--- common/src/main/java/net/spookly/moanimals/client/model/BirdModel.java
+++ common/src/main/java/net/spookly/moanimals/client/model/BirdModel.java
@@ -9,12 +9,16 @@ import org.jetbrains.annotations.NotNull;
 import net.spookly.moanimals.client.animations.BirdAnimations;
 import net.spookly.moanimals.entity.Bird;
 
+import net.minecraft.client.model.CowModel;
 import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.client.model.PigModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.animal.Cow;
 
 public class BirdModel<T extends Bird> extends HierarchicalModel<T> {
     // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
@@ -56,19 +60,15 @@ public class BirdModel<T extends Bird> extends HierarchicalModel<T> {
             .texOffs(16, 7).addBox(0.0F, -3.0F, 1.5F, 0.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
             .texOffs(16, 0).addBox(0.0F, -7.0F, -1.5F, 0.0F, 4.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.8F, -0.6F, -0.1745F, 0.0F, 0.0F));
 
-        PartDefinition wing_right = body.addOrReplaceChild("wing_right", CubeListBuilder.create().texOffs(0, 13).addBox(0.0F, 0.0F, -0.5F, 0.0F, 4.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.01F, -3.25F, 0.0F, 0.0F, 0.0F, -0.2618F));
-
-        PartDefinition wing_left = body.addOrReplaceChild("wing_left", CubeListBuilder.create().texOffs(0, 13).addBox(0.0F, 0.0F, -0.5F, 0.0F, 4.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.9999F, -3.25F, 0.0F, 0.0F, 0.0F, 0.3054F));
-
-        PartDefinition tail = body.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(0, 8).addBox(-1.5F, 0.0F, 0.0F, 3.0F, 0.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 1.5F));
+        body.addOrReplaceChild("wing_right", CubeListBuilder.create().texOffs(0, 13).addBox(0.0F, 0.0F, -0.5F, 0.0F, 4.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.01F, -3.25F, 0.0F, 0.0F, 0.0F, -0.2618F));
+        body.addOrReplaceChild("wing_left", CubeListBuilder.create().texOffs(0, 13).addBox(0.0F, 0.0F, -0.5F, 0.0F, 4.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.9999F, -3.25F, 0.0F, 0.0F, 0.0F, 0.3054F));
+        body.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(0, 8).addBox(-1.5F, 0.0F, 0.0F, 3.0F, 0.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 1.5F));
 
         PartDefinition leg_right = root.addOrReplaceChild("leg_right", CubeListBuilder.create().texOffs(22, 2).addBox(-0.5F, 0.0F, 0.0F, 1.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, -2.0F, 0.5F));
-
-        PartDefinition foot_right = leg_right.addOrReplaceChild("foot_right", CubeListBuilder.create().texOffs(10, 19).addBox(1.5F, 2.0F, -3.5F, 1.0F, 0.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 0.0F, 1.5F));
+        leg_right.addOrReplaceChild("foot_right", CubeListBuilder.create().texOffs(10, 19).addBox(1.5F, 2.0F, -3.5F, 1.0F, 0.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 0.0F, 1.5F));
 
         PartDefinition leg_left = root.addOrReplaceChild("leg_left", CubeListBuilder.create().texOffs(22, 0).addBox(-0.5F, 0.0F, 0.0F, 1.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.0F, -2.0F, 0.5F));
-
-        PartDefinition foot_left = leg_left.addOrReplaceChild("foot_left", CubeListBuilder.create().texOffs(18, 19).addBox(-0.5F, 1.0F, -1.5F, 1.0F, 0.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, -0.5F));
+        leg_left.addOrReplaceChild("foot_left", CubeListBuilder.create().texOffs(18, 19).addBox(-0.5F, 1.0F, -1.5F, 1.0F, 0.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, -0.5F));
 
         return LayerDefinition.create(meshdefinition, 32, 32);
     }
@@ -76,10 +76,12 @@ public class BirdModel<T extends Bird> extends HierarchicalModel<T> {
     @Override
     public void setupAnim(Bird entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
         this.root().getAllParts().forEach(ModelPart::resetPose);
+        this.applyHeadRotation(netHeadYaw, headPitch);
         this.animate(entity.idleAnimationState, BirdAnimations.idle, ageInTicks, 1f);
         this.animate(entity.dyingAnimationState, BirdAnimations.death, ageInTicks, 1f);
         this.animateWalk(BirdAnimations.walk, limbSwing, limbSwingAmount, 2f, 2.5f);
     }
+
 
     @Override
     public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, int color) {
@@ -89,5 +91,12 @@ public class BirdModel<T extends Bird> extends HierarchicalModel<T> {
     @Override
     public @NotNull ModelPart root() {
         return root;
+    }
+
+    private void applyHeadRotation(float headYaw, float headPitch) {
+        headYaw = Mth.clamp(headYaw, -30f, 30f);
+        headPitch = Mth.clamp(headPitch, -25f, 45f);
+        this.head.yRot = headYaw * ((float) Math.PI / 180F);
+        this.head.xRot = headPitch * ((float) Math.PI / 180F);
     }
 }
